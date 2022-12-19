9bdf51c3f7f290e52f47682996f9a7f465e022b1
April <buzdav2@gmail.com>
2022-12-19 10:51:28 -0800

    solve day2 part 2

--- day2.asm
+++ day2.asm
@@ -13,35 +13,64 @@ start
         JSR     open_file       ; load file into D4
 
         MOVEQ.L #0,D5           ; initial remaining data for reading from file
-        MOVEQ.L #0,D2           ; score accumulator
+        CLR.L   (part1_score).L ; also clears part2_score since they are adjacent words
 .count_score_loop
         JSR     get_rps_moves
         TST.W   D5
         BMI.S   .end_count_score_loop   ; if file has no more data, we are done
-        ;;;     add score based on move
-        ADDQ.W  #1,D2
+        ;;;     add points based on move (part 1)
+        ADDQ.W  #1,(part1_score).L
         EXT.W   D1
-        ADD.W   D1,D2
-        ;;;     add score based on if game was won
-        ;;;     calculate (opponent's guess - your guess) % 3
+        ADD.W   D1,(part1_score).L
+        ;;;     add points based on game result (part 2)
+        TST.B   D1
+        BEQ.S   .add_part1_result_points        ; 0 points for loss
+        ADDQ.W  #3,(part2_score).L              ; 3 points for tie
+        CMPI.B  #1,D1
+        BEQ.S   .add_part1_result_points
+        ADDQ.W  #3,(part2_score).L              ; 6 points (total) for win
+.add_part1_result_points
+        ;;;     add score based on game result (part 1)
+        ;;;     calculate (opponent's guess - your guess) % 3 into D2
         ;;;     0: tie
         ;;;     1: opponent won
         ;;;     2: you won
-        SUB.B   D1,D0
+        MOVE.B  D0,D2
+        SUB.B   D1,D2
         BPL.S   .diff_ok
-        ADDQ.B  #3,D0
+        ADDQ.B  #3,D2
 .diff_ok
-        CMPI.B  #1,D0
-        BEQ.S   .count_score_loop       ; add no score if opponent won
-        ADDQ.W  #3,D2
-        TST.B   D0
-        BEQ.S   .count_score_loop       ; add 3 if tie
-        ADDQ.W  #3,D2
-        BRA.S   .count_score_loop       ; add 6 total if win
+        CMPI.B  #1,D2
+        BEQ.S   .add_part2_move_points  ; 0 points for loss
+        ADDQ.W  #3,(part1_score).L
+        TST.B   D2
+        BEQ.S   .add_part2_move_points  ; 3 points for tie
+        ADDQ.W  #3,(part1_score).L
+.add_part2_move_points
+        ;;;     add score based on move you must make (part 2)
+        ;;;     Formula (simplified in code):
+        ;;;     (opponent's guess + result code - 1) % 3 + 1
+        MOVE.B  D0,D2
+        ADD.B   D1,D2
+        BNE.S   .p2_move_points_no_underflow
+        MOVEQ.L #3,D2
+        BRA.S   .p2_move_points_no_overflow
+.p2_move_points_no_underflow
+        CMPI.B  #3,D2
+        BLS.S   .p2_move_points_no_overflow
+        SUBQ.B  #3,D2
+.p2_move_points_no_overflow
+        EXT.W   D2
+        ADD.W   D2,(part2_score).L
+        BRA.S   .count_score_loop
 .end_count_score_loop
 
-        MOVE.W  D2,-(SP)
-        LEA.L   (msg_rps_score).L,A0
+        MOVE.W  (part1_score).L,-(SP)
+        LEA.L   (msg_rps_score_part1).L,A0
+        JSR     printf
+
+        MOVE.W  (part2_score).L,(SP)
+        LEA.L   (msg_rps_score_part2).L,A0
         JSR     printf
         ADDQ.L  #2,SP
 
@@ -84,5 +113,12 @@ get_rps_moves
 
         DATA
 input_file_name         DC.B    "input/day2.txt",0
-msg_rps_score           DC.B    "Score: %d",10,0
+msg_rps_score_part1     DC.B    "Total score (part 1): %d",10,0
+msg_rps_score_part2     DC.B    "Total score (part 2): %d",10,0
 emsg_invalid_line       DC.B    "Invalid RPS (line %d)",10,0
+
+        BSS
+                        EVEN
+;;;                     these must remain adjacent in memory
+part1_score             DS.W    1
+part2_score             DS.W    1
